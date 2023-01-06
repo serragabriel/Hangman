051bb8aa58035bc0a4991efee391b960d929fea9
Serra <gabrielserralola@gmail.com>
2023-01-06 14:37:07 -0300

    better format

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import random
 from words import words
 import string
 
+
 def get_valid_word(words):
     word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
     return word.upper()
+
 
 def hangman():
     word = get_valid_word(words)
@@ -18,11 +20,12 @@ def hangman():
 
     while len(word_letters) > 0 and lives > 0:
 
-        print("You have", lives, "lives left and you have used these letters:", ' '.join(used_letters))
+        print("You have", lives, "lives left and you have used these letters:",
+              ' '.join(used_letters))
 
-        world_list = [letter if letter in used_letters else '-' for letter in word]
+        world_list = [
+            letter if letter in used_letters else '-' for letter in word]
         print("Current word: ", ' '.join(world_list))
-
 
         user_letter = input("Guess a letter: ").upper()
         if user_letter in alphabet - used_letters:
@@ -34,15 +37,15 @@ def hangman():
                 lives = lives - 1
                 print("Letter is not in word. ")
 
-
         elif user_letter in used_letters:
-            print("You already typed this letter. Please try again")  
+            print("You already typed this letter. Please try again")
 
         else:
-            print("Invalid. Do it again")     
+            print("Invalid. Do it again")
     if lives == 0:
         print("You lost, GL next time. The word was", word)
     else:
-        print("You guessed the word", word, "!!!!!")                            
+        print("You guessed the word", word, "!!!!!")
+
 
 hangman()
